7063409091c8a841fe5f6e89b03c773d18282582
Saashvath <74235537+Saashvath@users.noreply.github.com>
2021-02-25 16:46:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var canvas = new fabric.Canvas('myCanvas')
+ super_hero_width = 40;
+ super_hero_height = 40;
+
+ player_object = [];
+ block_object = [];
+
+
+ function player_object(){
+
+    
+ }
